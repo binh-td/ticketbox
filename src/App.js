1445f67077f7fbaa99560396f69cb87d2ec36f02
Truong Binh <binhtd.com@gmail.com>
2019-09-12 10:51:50 +0700

    create project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchPage from './components/SearchPage';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchPage />
+    </div>
+  );
+}
+
+export default App;
