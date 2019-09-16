b2cead311d08bad73bd2869826138f85d6cdcf76
Nguyen Duc Lam <nguyenduclam1711@gmail.com>
2019-09-16 09:24:39 +0700

    import file xlxs

--- src/Page/TicketImport.js
+++ src/Page/TicketImport.js
@@ -1,30 +1,173 @@
 import React from "react"
-import { Button, Table } from "react-bootstrap"
+import { Form, Table, Button } from "react-bootstrap"
+import XLSX from "xlsx"
 
-const TicketImport = props => {
-  return (
-    <div className="container">
-      <h1>Quản lý import vé</h1>
-      <div>
-        <Button variant="primary" style={{ margin: "2rem"}}>Import Utop</Button>
-        <Button variant="primary" style={{ margin: "2rem"}}>Import TicketGo</Button>
-        <Button variant="primary" style={{ margin: "2rem"}}>Import TicketBox</Button>
+class TicketImport extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      file: {},
+      ticketDatas: [],
+    }
+    this.importOnChangeHandle = this.importOnChangeHandle.bind(this)
+    this.deleteBtnOnClick = this.deleteBtnOnClick.bind(this)
+  }
+
+  importOnChangeHandle(e) {
+    const file = e.target.files
+    const date = new Date()
+    const dateTime = date.toLocaleString('vi-VN')
+
+    if (file && file[0]) {
+      this.setState({
+        file: file[0],
+        ticketDatas: [
+          ...this.state.ticketDatas,
+          {
+            timeUpload: dateTime,
+            saleChannel: e.target.name,
+            fileName: file[0].name,
+            data: [],
+          }
+        ]
+      })
+    }
+  }
+
+  deleteBtnOnClick(index) {
+    this.setState({
+      ticketDatas: this.state.ticketDatas.filter((detail, i) => i !== index)
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.file !== prevState.file) {
+      const reader = new FileReader(),
+        rABS = !!reader.readAsBinaryString
+      let ticketData = this.state.ticketDatas.pop()
+
+      reader.onload = e => {
+        /* Parse data */
+        const bstr = e.target.result
+        const wb = XLSX.read(bstr, {
+          type: rABS ? "binary" : "array",
+          bookVBA: true
+        })
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0]
+        const ws = wb.Sheets[wsname]
+        /* Convert array of arrays */
+        const data = XLSX.utils.sheet_to_json(ws)
+        ticketData = {
+          ...ticketData,
+          data: data,
+        }
+        /* Update state */
+        this.setState({
+          ticketDatas: [
+            ...this.state.ticketDatas,
+            ticketData
+          ]
+        })
+      }
+
+      if (rABS) {
+        reader.readAsBinaryString(this.state.file)
+      } else {
+        reader.readAsArrayBuffer(this.state.file)
+      }
+    }
+  }
+
+  render() {
+    console.log(this.state.ticketDatas)
+    return (
+      <div className="container">
+        <h1>Quản lý import vé</h1>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              name="Utop"
+              type="file"
+              id="custom-input-Utop-file"
+              onChange={this.importOnChangeHandle}
+              accept=".csv, .xlsx, .xls"
+            />
+            <Form.Label
+              htmlFor="custom-input-Utop-file"
+              className="btn btn-primary"
+              style={{ margin: "0 2rem 0 0" }}
+            >
+              Import Utop
+            </Form.Label>
+            <Form.Control
+              name="TicketBox"
+              type="file"
+              id="custom-input-TicketBox-file"
+              onChange={this.importOnChangeHandle}
+              accept=".csv, .xlsx, .xls"
+            />
+            <Form.Label
+              htmlFor="custom-input-TicketBox-file"
+              className="btn btn-primary"
+              style={{ margin: "0 2rem" }}
+            >
+              Import TicketBox
+            </Form.Label>
+            <Form.Control
+              name="TicketGo"
+              type="file"
+              id="custom-input-TicketGo-file"
+              onChange={this.importOnChangeHandle}
+              accept=".csv, .xlsx, .xls"
+            />
+            <Form.Label
+              htmlFor="custom-input-TicketGo-file"
+              className="btn btn-primary"
+              style={{ margin: "0 2rem" }}
+            >
+              Import TicketGo
+            </Form.Label>
+          </Form.Group>
+        </Form>
+        <Table bordered hover>
+          <thead>
+            <tr>
+              <th>Ngày Import</th>
+              <th>Tên file</th>
+              <th>Kênh bán</th>
+              <th>Số lượng đơn</th>
+              <th>Tổng tiền</th>
+              <th>Thao tác</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.ticketDatas.map((detail, index) => (
+              <>
+                <tr key={index}>
+                  <td>{detail.timeUpload}</td>
+                  <td>{detail.fileName}</td>
+                  <td>{detail.saleChannel}</td>
+                  <td>{detail.data.length}</td>
+                  <td></td>
+                  <td>
+                    <Button variant="primary">Chi tiết</Button>
+                    <Button
+                      id="deleteBtn"
+                      onClick={() => this.deleteBtnOnClick(index)}
+                      style={{ margin: "0 0 0 2rem" }}
+                    >
+                      Xóa
+                    </Button>
+                  </td>
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </Table>
       </div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Ngày Import</th>
-            <th>Tên file</th>
-            <th>Kênh bán</th>
-            <th>Số lượng đơn</th>
-            <th>Tổng tiền</th>
-            <th>Thao tác</th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </Table>
-    </div>
-  )
+    )
+  }
 }
 
 export default TicketImport
